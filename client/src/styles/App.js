b01967a1420369d0b9282601aaae7fcf8e5bc928
SLAERZA <149735220+SLAERZA@users.noreply.github.com>
2025-05-21 21:12:59 -0400

    Fix relative path to global.css

--- client/src/styles/App.js
+++ client/src/styles/App.js
@@ -2,17 +2,4 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Upload from "../pages/Upload";
-import './styles/global.css';
-
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/upload" element={<Upload />} />
-            </Routes>
-        </BrowserRouter>
-    );
-}
-
-export default App;
+import "../styles/global.css";
