07c69f5008fbdf61fc35172f155140ebbd5ad089
SLAERZA <149735220+SLAERZA@users.noreply.github.com>
2025-05-21 21:19:31 -0400

    Fix global.css path in correct App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
-import "./styles/global.css";
+import "../styles/global.css";
 
 function App() {
     return (
