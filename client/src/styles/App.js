cf8ec2ccca8d3dc578f5f2b0fb10fc52a94f947d
SLAERZA <149735220+SLAERZA@users.noreply.github.com>
2025-05-21 21:53:51 -0400

    Remove incorrect App.js from styles folder

--- client/src/styles/App.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Upload from "../pages/Upload";
-import "../styles/global.css";
